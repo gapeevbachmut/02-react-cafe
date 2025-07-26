2383a24e2afc2d6b3639a79037e700d5b19ec6a2
Андрій <gapeev1979@gmail.com>
2025-07-26 15:53:19 +0300

    fix neveruse

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,6 +1,4 @@
 import css from './Notification.module.css';
-// import
-import VoteStats from '../VoteStats/VoteStats';
 
 export default function Notification() {
   return <p className={css.message}>No feedback yet</p>;
